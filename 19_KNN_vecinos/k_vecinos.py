9b95d0b2fd10414f0f758d419926dccc18d48c8f
jamarillo <amarillo1983@gmail.com>
2025-03-18 17:41:42 +0100

    Algoritmo knn

---
+++ 19_KNN_vecinos/k_vecinos.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+X = iris.data
+y = iris.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+k= 3
+knn = KNeighborsClassifier(n_neighbors=k)
+knn.fit(X_train, y_train)
+
+y_pred = knn.predict(X_test)
+
+print(classification_report(y_test, y_pred))
+print(confusion_matrix(y_test, y_pred))
+
+for k in range(1,10):
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_train, y_train)
+    y_pred = knn.predict(X_test)
+    print(f"Predicción para {k} vecinos es: {y_pred}")
+    print(f"El valor de y_tests es       : {y_test}")
+    print(f"El valor de X_tests es       : {X_test}")
+    #print(f"k={k}, Accuracy: {knn.score(y_test, y_pred)}")
+
+
+
